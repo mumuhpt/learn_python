b6d34e862c8badb9b14dc7da0f6ffa2288d495f9
Murilo Terra <muriloterracontato@gmail.com>
2026-02-27 19:37:39 -0300

    Add files via upload

---
+++ verifica_par_ou_impar.py
@@ -0,0 +1,10 @@
+# Vamos criar um programa que verifica se um numero é par ou impar
+
+numero = int(input("Digite um numero: "))
+
+if numero % 2 == 0:
+    print("O numero é par")
+else:   
+    print("O numero é impar")
+
+    
