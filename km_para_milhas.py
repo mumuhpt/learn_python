a3a92467b15b5baa4712f9032ba20ce54b42e754
Murilo Terra <muriloterracontato@gmail.com>
2026-02-28 20:11:00 -0300

    Add files via upload

---
+++ km_para_milhas.py
@@ -0,0 +1,12 @@
+# Programa para converter Kilometros para Milhas 
+# Conversor de km para milhas é 1 Km = 0.621371
+print('\nConversor de KM para Milhas!')
+
+km = float(input('Digite a distancia em KM que precisa ser convertida: '))
+conversor = 0.621371
+milhas = km * conversor
+
+print(f'A distancia {km} KM é igual a {milhas}')
+
+
+
